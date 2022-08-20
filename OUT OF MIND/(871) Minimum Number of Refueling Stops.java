12a264982a55fa4f4e8b440774d0223662fdcdf1
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-08-20 13:18:00 +0530

    out of mind

---
+++ OUT OF MIND/(871) Minimum Number of Refueling Stops.java
@@ -0,0 +1,20 @@
+class Solution {
+    //nhi hua mere se ye
+    //out of mind 
+    //dubara aaunga ispe
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int i = 0, count = 0, far = startFuel;
+        while(far < target){
+            while(i<stations.length && stations[i][0] <= far){
+                pq.add(stations[i][1]);
+                i++;
+            }
+            if(pq.size()==0)
+                return -1;
+            far += pq.remove();
+            count++;
+        }
+        return count; 
+    }
+}
