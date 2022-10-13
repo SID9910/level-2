84de8ea359ccbf4fd83be7acabf125500680f80b
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-10-13 12:09:46 +0530

    array

--- ARRAY/920 ·(Lintcode) Meeting Rooms.java
+++ ARRAY/920 ·(Lintcode) Meeting Rooms.java
@@ -12,38 +12,38 @@
 public class Solution {
     /**
      * @param intervals: an array of meeting time intervals
-     * @return: the minimum number of conference rooms required
+     * @return: if a person could attend all meetings
      */
-    public int minMeetingRooms(List<Interval> intervals) {
-      if(intervals.size()<=1){
-        return 1;
-      }
-      int[] start = new int[intervals.size()];
-      int[] end =new int[intervals.size()];
-     int i=0;
-       for(Interval val:intervals){
-          start[i] =val.start;
+    public boolean canAttendMeetings(List<Interval> intervals) {
+   if(intervals.size()<=1){
+       return true;
+   }
+     int[] start = new int[intervals.size()];
+      int[] end = new int[intervals.size()];
+      int i=0;
+    for(Interval val:intervals){
+          start[i]=val.start;
           end[i]=val.end;
           i++;
       }
       Arrays.sort(start);
       Arrays.sort(end);
- 
       i=0;
-     int  j=0;
-int count =0;
-int res=Integer.MIN_VALUE;
+      int j=0;
+      int count=0;
+      int res =Integer.MIN_VALUE;
       while(i<start.length && j<end.length){
           if(start[i]<end[j]){
-             count++;
-             i++;
+              i++;
+              count++;
           }else{
-              count--;
               j++;
+              count--;
           }
-res=Math.max(count,res);
+          res=Math.max(count,res);
       }
-
-return res;
+     
+       return res==1;
+ 
     }
-}   
+}
