dba60301fba65e497f1643a09fc37dde37be79a3
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-10-15 10:19:46 +0530

    array

---
+++ ARRAY/(179) Largest Number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        //convert into string
+      String[] str = new String[nums.length];
+        for(int i=0;i<nums.length;i++){
+            str[i]=nums[i]+"";
+        }
+        //sort on the basis so that large values come first
+        Arrays.sort(str,(a,b)->{
+            String case1 =a+b;
+            String case2 =b+a;
+           return case2.compareTo(case1); 
+        });
+        
+        //if starting zero encounter then return 0
+        if(str[0].equals("0")){
+            return "0";
+        }
+        
+        //then append to stringbuilder
+        StringBuilder sb = new StringBuilder();
+        for(String val:str){
+            sb.append(val);
+        }
+        return sb.toString();
+    }
+}
