9a9c085a5c168e3b8101c0a208a2ce92e49b1eb8
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-10-16 18:48:43 +0530

    hahsmap

---
+++ HASHMAP/(409) Longest Palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    
+    //hashmao ke baad dubara karunga ye
+    public int longestPalindrome(String s) {
+        Map<Character,Integer> counts = new HashMap();
+        //frequency daal do
+        for(char c:s.toCharArray()){
+            counts.put(c,counts.getOrDefault(c,0)+1);
+        }
+        int result =0;
+        boolean odd_found =false;
+        for(int c:counts.values()){
+            if(c%2 ==0){
+                result+=c; //adding even
+            }else{
+                odd_found =true;
+                result+=c-1; //adding odd
+            }
+        }
+        //add one at the end
+        if(odd_found){
+            result++;
+        }
+        return result;
+    }
+}
