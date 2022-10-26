4e42fba8f39d294d9a13a1762d21615abe5a0f47
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-10-26 11:01:51 +0530

    HashMap

---
+++ HASHMAP/(523) Continuous Subarray Sum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+       // stores mod values of k mapped with first time found index
+       HashMap<Integer,Integer> map = new HashMap<>();
+       long sum=0;
+       map.put(0,-1); // base case no sum at -1 position
+       for(int i=0;i<nums.length;i++){
+           sum+=nums[i];
+           int rem = (int)sum%k;
+           if(map.containsKey(rem)){
+               
+               if(i-map.get(rem)>1)
+                   return true;
+           }
+           else
+               map.put(rem,i);
+       }
+       return false;
+   }
+}
+
