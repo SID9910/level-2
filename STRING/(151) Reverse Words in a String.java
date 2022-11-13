83862cb19d67124ec935dbdbefcd02683903340b
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-11-13 11:58:19 +0530

    string

---
+++ STRING/(151) Reverse Words in a String.java
@@ -0,0 +1,30 @@
+class Solution {
+    // public String reverseWords(String s) {
+    //     String[] words =s.trim().split(" +");
+    //     Collections.reverse(Arrays.asList(words));
+    //     return String.join(" " ,words);
+    // }
+
+
+//two pointer approach
+     public String reverseWords(String s) {
+     StringBuilder result = new StringBuilder();
+     for(int i=0;i<s.length();i++){
+        if(s.charAt(i)==' '){
+            continue;
+        }
+        int start =i;
+        while(i<s.length() && s.charAt(i)!=' '){
+            i++;
+        }
+        if(result.length()==0){
+            result.insert(0,s.substring(start ,i));
+
+        }else{
+            result.insert(0," ").insert(0,s.substring(start ,i));
+        }
+     }
+     return result.toString();
+     
+        }
+}
