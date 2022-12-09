3dd2eb07ea0a31ae7f51a861e317e71c7d38a25d
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-12-09 17:54:44 +0530

    DP

--- DP/(198) House Robber.java
+++ DP/(198) House Robber.java
@@ -1,18 +1,17 @@
 class Solution {
-    //level-1 dp(Maximum sum non adjacent elements)
-    //imp  hai
-    public int rob(int[] nums) {
-       int inc =nums[0];
-        int exc =0;
-        for(int i=1;i<nums.length;i++){
-            int ninc =exc+nums[i];
-            int nexc =Math.max(inc,exc);
-            
-            inc =ninc;
-            exc=nexc;
-        }
-        int ans =Math.max(inc,exc);
-        
-        return ans;
-    }
+    // dp(Maximum sum non adjacent elements)
+   public int rob(int[] nums) {
+     int n =nums.length;
+       int prev =nums[0];
+       int prev2 =0;
+       for(int i=1;i<n;i++){
+           int take =nums[i];
+           if(i>1)take+=prev2;
+           int nontake =0+prev;
+           int curri =Math.max(take,nontake);
+           prev2 =prev;
+           prev=curri;
+       }
+       return prev;
+   }
 }
---
+++ DP/(213) House Robber II.java
@@ -0,0 +1,41 @@
+class Solution {
+    //roobber 2
+    public int rob(int[] nums) {
+       ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        int n =nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        for(int i=0;i<nums.length;i++){
+            if(i!=0){
+                list1.add(nums[i]);
+            }
+            if(i!=n-1){
+                list2.add(nums[i]);
+            }
+        }
+        return Math.max(helper(list1),helper(list2));
+    }
+    
+     public int helper(ArrayList<Integer> house){
+           int n =house.size();
+       int prev =house.get(0);
+       int prev2 =0;
+       for(int i=1;i<n;i++){
+           int take =house.get(i);
+           if(i>1)take+=prev2;
+           int nontake =0+prev;
+           int curri =Math.max(take,nontake);
+           prev2 =prev;
+           prev=curri;
+       }
+       return prev; 
+     }
+    
+}
+
+
+    
+   
+    
