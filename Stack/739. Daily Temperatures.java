81ef37f3209ddc5a5f35e7e58f731ad9de13d8fd
Siddharth Gupta <siddharthgupta052@gmail.com>
2022-12-18 11:52:08 +0530

    stack

---
+++ Stack/739. Daily Temperatures.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+     Stack<Integer> st = new Stack<>();
+        int n =temperatures.length;
+        int[] ans = new int[temperatures.length];
+        
+        for(int i=n-1;i>=0;i--){
+            
+            
+         while(st.size()>0 && temperatures[i]>=temperatures[st.peek()]){
+             st.pop();
+         }            
+            
+            if(st.size()>0){
+                ans[i]=st.peek()-i;
+            }
+            
+            st.push(i);
+            
+            
+            
+            
+        }
+        return ans;
+        
+        
+    }
+}
