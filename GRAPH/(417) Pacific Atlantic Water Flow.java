9871696e7a4843017217154150f087d753d7937f
Siddharth Gupta <siddharthgupta052@gmail.com>
2023-03-05 19:00:30 +0530

    hello

--- GRAPH/(417) Pacific Atlantic Water Flow.java
+++ GRAPH/(417) Pacific Atlantic Water Flow.java
@@ -1,4 +1,5 @@
 class Solution {
+    //not that easy
     public List<List<Integer>> pacificAtlantic(int[][] matrix) {
         List<List<Integer>> result = new ArrayList<>();
         int row = matrix.length;
