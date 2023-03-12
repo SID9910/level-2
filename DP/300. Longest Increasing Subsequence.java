ef064cdae9286360cb0aa572734bd4ed23f60e98
Siddharth Gupta <siddharthgupta052@gmail.com>
2023-03-12 12:01:06 +0530

    dp

--- DP/300. Longest Increasing Subsequence.java
+++ DP/300. Longest Increasing Subsequence.java
@@ -19,26 +19,46 @@ class Solution {
 //     }
  
     //memoization
-       public int lengthOfLIS(int[] nums) {
-           int[][] dp = new int[nums.length][nums.length+1];
-      int length=longest(0 ,-1 ,nums ,dp);
-        return length;
-    }
-    public int longest(int index ,int prev_index ,int[] nums,int[][] dp){
+//        public int lengthOfLIS(int[] nums) {
+//            int[][] dp = new int[nums.length][nums.length+1];
+//       int length=longest(0 ,-1 ,nums ,dp);
+//         return length;
+//     }
+//     public int longest(int index ,int prev_index ,int[] nums,int[][] dp){
         
-        if(index ==nums.length){
-            return 0;
-        }
-        if(dp[index][prev_index+1]!=0){
-            return dp[index][prev_index+1];
-        }
-        //not take
-        int len =0+longest(index+1 ,prev_index , nums,dp);
+//         if(index ==nums.length){
+//             return 0;
+//         }
+//         if(dp[index][prev_index+1]!=0){
+//             return dp[index][prev_index+1];
+//         }
+//         //not take
+//         int len =0+longest(index+1 ,prev_index , nums,dp);
+//         if(prev_index==-1 || nums[index]>nums[prev_index]){
+//             //take
+//             len =Math.max(len ,1+longest(index+1 ,index , nums,dp));
+//         }
+//         return dp[index][prev_index+1] = len;
+//     }
+    
+    //tabulation
+           public int lengthOfLIS(int[] nums) {
+           int[][] dp = new int[nums.length+1][nums.length+1];
+               int n=nums.length;
+               for(int index =n-1;index>=0;index--){
+                   for(int prev_index=index-1;prev_index>=-1;prev_index--){
+                     int len =0+dp[index+1][prev_index+1];
         if(prev_index==-1 || nums[index]>nums[prev_index]){
             //take
-            len =Math.max(len ,1+longest(index+1 ,index , nums,dp));
+            len =Math.max(len ,1+dp[index+1][index+1]);
         }
-        return dp[index][prev_index+1] = len;
+                       
+                       dp[index][prev_index+1] = len;
+                   }
+               }
+               return  dp[0][-1+1];
+
     }
+
 
 }
