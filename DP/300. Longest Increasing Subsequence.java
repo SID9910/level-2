d086061da7852e88490f96c41468c8f36fe4a2a3
Siddharth Gupta <siddharthgupta052@gmail.com>
2023-03-12 12:29:15 +0530

    dp

--- DP/300. Longest Increasing Subsequence.java
+++ DP/300. Longest Increasing Subsequence.java
@@ -42,22 +42,42 @@ class Solution {
 //     }
     
     //tabulation
-           public int lengthOfLIS(int[] nums) {
-           int[][] dp = new int[nums.length+1][nums.length+1];
-               int n=nums.length;
-               for(int index =n-1;index>=0;index--){
-                   for(int prev_index=index-1;prev_index>=-1;prev_index--){
-                     int len =0+dp[index+1][prev_index+1];
-        if(prev_index==-1 || nums[index]>nums[prev_index]){
-            //take
-            len =Math.max(len ,1+dp[index+1][index+1]);
-        }
+//            public int lengthOfLIS(int[] nums) {
+//            int[][] dp = new int[nums.length+1][nums.length+1];
+//                int n=nums.length;
+//                for(int index =n-1;index>=0;index--){
+//                    for(int prev_index=index-1;prev_index>=-1;prev_index--){
+//                      int len =0+dp[index+1][prev_index+1];
+//         if(prev_index==-1 || nums[index]>nums[prev_index]){
+//             //take
+//             len =Math.max(len ,1+dp[index+1][index+1]);
+//         }
                        
-                       dp[index][prev_index+1] = len;
-                   }
-               }
-               return  dp[0][-1+1];
+//                        dp[index][prev_index+1] = len;
+//                    }
+//                }
+//                return  dp[0][-1+1];
 
+//     }
+    
+    //more optimised tabulation
+      public int lengthOfLIS(int[] nums) {
+           int[] dp = new int[nums.length];
+          for(int i=0;i<dp.length;i++){
+              dp[i]=1;
+          }
+          int maxi =1; //khud ho sakta hai
+          for(int index=0;index<nums.length;index++){
+              for(int prev=0;prev<index;prev++){
+                  if(nums[prev]<nums[index]){
+                      dp[index]=Math.max(1+dp[prev],dp[index]);
+                  }
+              }
+              maxi =Math.max(maxi ,dp[index]);
+              
+          }
+             
+          return maxi;
     }
 
 
