9ee7167160821823ab2261d4bf72157b421c9562
Siddharth Gupta <siddharthgupta052@gmail.com>
2023-05-01 13:17:41 +0530

    Hashmap

---
+++ HASHMAP/Longest Consecutive 1's.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class Solution{
+    
+    /*  Function to calculate the longest consecutive ones
+    *   N: given input to calculate the longest consecutive ones
+    */
+    public static int maxConsecutiveOnes(int N) {
+        
+       String s=Integer.toBinaryString(N);
+       int max=0,count=0;
+       for(char ch:s.toCharArray()){
+           
+           if(ch=='1'){
+           count++;
+           
+            max=Math.max(max,count);
+           }
+           else 
+           count=0;
+          
+       }
+        return max;
+        
+    }
+}
+
+
+
+//{ Driver Code Starts.
+
+class Main {
+    
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();//testcases
+		while(t-->0){
+		    int n = sc.nextInt();//input n
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxConsecutiveOnes() function
+		    System.out.println(obj.maxConsecutiveOnes(n));
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
