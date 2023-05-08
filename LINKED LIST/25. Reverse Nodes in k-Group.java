0d44d5d63ad0212122267308a5c9434dae003cca
Siddharth Gupta <siddharthgupta052@gmail.com>
2023-05-08 22:14:02 +0530

    HAShMAp

--- LINKED LIST/25. Reverse Nodes in k-Group.java
+++ LINKED LIST/25. Reverse Nodes in k-Group.java
@@ -9,7 +9,7 @@
  * }
  */
 class Solution {
-    //tc-o(n) space=0(n)
+    //tc-o(n) space=0(n) hai
     public ListNode reverseKGroup(ListNode head, int k) {
        if(head==null){
            return null;
