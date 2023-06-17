9842e78e526b25b29dd5960227977b2404bdbab2
SIDDHARTH GUPTA <95719152+SID9910@users.noreply.github.com>
2023-06-17 13:06:03 +0530

    GRaph

---
+++ Graph(Striver)/1020. Number of Enclaves.java
@@ -0,0 +1,63 @@
+ class Pair{
+     int first;
+     int second;
+     Pair (int f1 ,int f2){
+         this.first =f1;
+         this.second =f2;
+     } 
+ }
+class Solution {
+    public int numEnclaves(int[][] grid) {
+       
+        int n=grid.length;
+        int m=grid[0].length;
+     
+         Queue<Pair> que = new LinkedList<Pair>();
+        int[][] vis =new int[n][m];
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(i==0 ||j==0 ||i==n-1 || j==m-1 )
+                if(grid[i][j]==1){
+                    que.add(new Pair(i,j));
+                    vis[i][j]=1;
+                }
+                
+            }
+        }
+        int[] delrow ={-1,0,1,0};
+        int[] delcol ={0,1,0,-1};
+         
+        while(!que.isEmpty()){
+            
+            int row =que.peek().first;
+            int col =que.peek().second;
+                que.remove();
+            
+            for(int i=0;i<4;i++){
+               int nrow =row+delrow[i];
+                int ncol =col+delcol[i];
+                
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                    que.add(new Pair(nrow ,ncol));
+                    vis[nrow][ncol]=1;
+                }
+                
+            }
+            
+            
+        }
+     
+        int count =0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0 && grid[i][j]==1){
+                    count++;
+                }
+            }
+        }
+        return  count;
+        
+    }
+}
