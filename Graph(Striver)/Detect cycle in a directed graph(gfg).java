0b125745469fc642d98903768eb5b6821a2658cf
SIDDHARTH GUPTA <95719152+SID9910@users.noreply.github.com>
2023-06-19 18:12:16 +0530

    GRaph

--- Graph(Striver)/Detect cycle in a directed graph(gfg).java
+++ Graph(Striver)/Detect cycle in a directed graph(gfg).java
@@ -33,39 +33,81 @@ class DriverClass {
 
 class Solution {
     // Function to detect cycle in a directed graph.
-    public boolean isCyclic(int v, ArrayList<ArrayList<Integer>> adj) {
+    // public boolean isCyclic(int v, ArrayList<ArrayList<Integer>> adj) {
 
-      int[] vis = new int[v];
-      int[] pathvis =new int[v];
+    //   int[] vis = new int[v];
+    //   int[] pathvis =new int[v];
       
-      for(int i=0;i<v;i++){
+    //   for(int i=0;i<v;i++){
           
-          if(vis[i]==0){
-              if(dfscheck(i,adj ,vis ,pathvis)==true){
-                  return true;
-              }
-          }
-      }
-      return false;
-    }
+    //       if(vis[i]==0){
+    //           if(dfscheck(i,adj ,vis ,pathvis)==true){
+    //               return true;
+    //           }
+    //       }
+    //   }
+    //   return false;
+    // }
     
-    public boolean dfscheck(int node ,ArrayList<ArrayList<Integer>> adj ,int[] vis ,int[] pathvis){
-        vis[node]=1;
-        pathvis[node]=1;
+    // public boolean dfscheck(int node ,ArrayList<ArrayList<Integer>> adj ,int[] vis ,int[] pathvis){
+    //     vis[node]=1;
+    //     pathvis[node]=1;
         
-        for(int it:adj.get(node)){
-            if(vis[it]==0){
-                if(dfscheck(it ,adj ,vis, pathvis)==true){
-                    return true;
-                }
-                }else if(pathvis[it]==1){
-                    return true;
-                }
+    //     for(int it:adj.get(node)){
+    //         if(vis[it]==0){
+    //             if(dfscheck(it ,adj ,vis, pathvis)==true){
+    //                 return true;
+    //             }
+    //             }else if(pathvis[it]==1){
+    //                 return true;
+    //             }
             
-        }
-        pathvis[node]=0;
+    //     }
+    //     pathvis[node]=0;
         
-         return false;
+    //      return false;
+        
+    // }
+    
+    //KHANS TOPOLOGICAL  SORT BFS
+     public boolean isCyclic(int v, ArrayList<ArrayList<Integer>> adj)
+    {
+        int[] indegree = new int[v];
+        
+        for(int i=0;i<v;i++){
+            for(int it:adj.get(i)){
+                
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> que = new LinkedList<>();
+        
+        for(int i=0;i<v;i++){
+            if(indegree[i]==0){
+                que.add(i);
+            }
+        }
+        
+        int count =0;
+        int i=0;
+        
+        while(!que.isEmpty()){
+            int node  =que.peek();
+            que.remove();
+            count++;
+            
+            for(int it:adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    que.add(it);
+                }
+            }
+        }
+        if(count==v){
+            return false;
+        }
+        return true;
         
     }
+
 }
