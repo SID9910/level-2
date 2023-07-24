c37cff660d02ba2d2dae3d6b0c493b62cbb627f4
SIDDHARTH GUPTA <95719152+SID9910@users.noreply.github.com>
2023-07-24 10:25:37 +0530

    GRaph

--- Graph(Striver)/200. Number of Islands.java
+++ Graph(Striver)/200. Number of Islands.java
@@ -61,4 +61,43 @@ public void bfs(int row,int col,int[][] vis,char[][] grid){
          }
          }
 
+          //main khud isse dfs se kara hai eek or approach
+    //assume 4 directions only
+    public int numIslands(char[][] grid) {
+     int n=grid.length;
+     int m=grid[0].length;
+     
+        int[][] vis = new int[n][m];
+        
+        int count =0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(grid[i][j]=='1' && vis[i][j]==0){
+                    count++;
+                    dfs(i ,j ,grid ,vis);
+                }
+            }
+        }
+        return count;
+    }
+    
+    public static  void dfs(int i ,int j ,char[][] grid ,int[][] vis){
+             int n=grid.length;
+     int m=grid[0].length;
+        vis[i][j]=1;
+        
+        for(int delr=-1;delr<=1;delr++){
+            for(int delc=-1;delc<=1;delc++){
+                int r =i+delr;
+                int c =j+delc;
+                
+                if( Math.abs(delr-delc)==1 && r>=0 && r<n && c>=0 && c<m && vis[r][c]==0 && grid[r][c]=='1'){
+                    dfs(r,c,grid ,vis);
+                }
+            }
+        }
+        
+        }
+
 }
