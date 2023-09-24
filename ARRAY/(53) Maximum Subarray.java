e8205a55ffe1ac6e628a5a8fb77b95b2c21815ca
SIDDHARTH GUPTA <95719152+SID9910@users.noreply.github.com>
2023-09-24 19:53:43 +0530

    NEWS Daily

--- ARRAY/(53) Maximum Subarray.java
+++ ARRAY/(53) Maximum Subarray.java
@@ -1,34 +1,85 @@
 class Solution {
+    //brute force
+    
+    // public int maxSubArray(int[] nums){
+    // int max =Integer.MIN_VALUE;
+    // for(int i=0;i<nums.length;i++){
+    //  for(int j=i;j<nums.length;j++){
+    //      int sum=0;
+    //      for(int k=i;k<=j;k++){
+    //          sum+=nums[k];
+    //          max =Math.max(max ,sum);
+    //      }
+    //  }   
+    // }
+    //     return max;
+    // }
+    
+    //better 
+//     public int maxSubArray(int[] nums){
+//     int max =Integer.MIN_VALUE;
+//     for(int i=0;i<nums.length;i++){
+//         int sum=0;
+//         for(int j=i;j<nums.length;j++){
+        
+         
+//              sum+=nums[j];
+//              max =Math.max(max ,sum);
+//          }
+//      }
+//         return max;
+//     }
+        
+    
+    //optimised
+    
+    //kadanes algorithm very imp
     public int maxSubArray(int[] nums) {
-     //kadanes algorithm very imp
+     
         int res =Integer.MIN_VALUE;
         int sum=0;
         for(int i=0;i<nums.length;i++){
-            if(sum<0){
-                sum=nums[i];
-            }else{
-                sum+=nums[i];
+            sum+=nums[i];
+            if(sum>res){
+                res =sum;
             }
-            res =Math.max(res,sum);
+            
+            if(sum<0){
+                sum=0;
+            }
         }
         return res;
-
-    
-       //brute force tle n2
-//       int n = nums.length;
-//         int maxSum = Integer.MIN_VALUE;
-//         for(int i=0;i<n;i++){
-//             int sum = nums[i];
-//             if(sum>maxSum)
-//                 maxSum = sum;
-//             for(int j=i+1;j<n;j++){
-//                 sum+=nums[j];
-//                 if(sum>maxSum)
-//                     maxSum = sum;
+}
+   
+    //kadane's ke throug print bhi kar sakte hai
+    //aagr maximum subarray print ke ley boll dea taab ye karna
+//     public int maxSubArray(int[] nums) {
+     
+//         int res =Integer.MIN_VALUE;
+//         int sum=0;
+//         int start =-1;
+//         int ansstart =-1;
+//         int end =-1;
+//         for(int i=0;i<nums.length;i++){
+        
+//             if(sum==0){
+//                 start =i;
+//             }
+//             sum+=nums[i];
+        
+//             if(sum>res){
+//                 res =sum;
+//                 ansstart =start;
+//                 end =i;
+//             }
+            
+//             if(sum<0){
+//                 sum=0;
 //             }
 //         }
-        
-//         return maxSum;                
-
-}
+//         for(int i=ansstart ;i<=end;i++){
+//             System.out.print(nums[i]);
+//         }
+//         return res;
+// }
 }
